980752c738a8d04d912a830d101bbe5495b1e5c8
OussamaAtifii <oussamaati03@gmail.com>
2024-11-11 22:37:28 +0100

    feat(backend):  add self-calling function for health checks

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,12 +7,11 @@ import cookieParser from 'cookie-parser';
 import paymentMethodRoute from './routes/paymentMethodsRoutes';
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 app.use(
   cors({
     origin: ['http://localhost:5173', 'https://budget-bee.oussamaati.dev'],
-
     credentials: true,
   })
 );
@@ -28,6 +27,19 @@ app.use('/transaction', transactionRouter);
 app.use('/category', categoriesRouter);
 app.use('/payment-method', paymentMethodRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function callSelf() {
+  setInterval(async () => {
+    try {
+      await fetch(`http://localhost:${PORT}`);
+      console.log('Autocall done');
+    } catch (error) {
+      console.error('Error in autocall:', error);
+    }
+  }, 5 * 60 * 1000);
+}
+
+callSelf();
